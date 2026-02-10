27224073989946852456a8440e0d83841522df17
kralexey15-art <kraleey15@gmail.com>
2026-02-10 14:41:40 +0100

    обнова папки src

---
+++ src/practice/prcForWhile.py
@@ -0,0 +1,15 @@
+# for i in range(1, 6, 2):
+#     print(i)
+
+# count = 0
+# word = "Hello World!"
+# for i in word:
+#     if i == "l":
+#         count += 1
+
+# print("Count: ", count)
+
+isHasCar = True
+while isHasCar:
+    if input("Enter data: ") == "stop":
+        isHasCar = False
